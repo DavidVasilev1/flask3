03c3f45cf41a430a5ccb6e569316ce43eb491b95
DavidVasilev1 <davidkvasilev@gmail.com>
2023-03-26 14:13:30 -0700

    wertwert

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,8 @@ COPY . /
 RUN pip install --no-cache-dir -r requirements.txt
 RUN pip install gunicorn
 
-ENV GUNICORN_CMD_ARGS="--workers=1 --bind=0.0.0.0:8199"
+ENV GUNICORN_CMD_ARGS="--workers=1 --bind=0.0.0.0:8200"
 
-EXPOSE 8199
+EXPOSE 8200
 
 CMD [ "gunicorn", "main:app" ]
--- main.py
+++ main.py
@@ -5,10 +5,7 @@ from saakd_api.api.todo import todo_bp
 
 from saakd_api.model.todos import init_todos
 
-
 app.register_blueprint(todo_bp)
-
-
 
 @app.before_first_request
 def init_db():
@@ -21,4 +18,4 @@ def init_db():
 if __name__ == "__main__":
     cors = CORS(app)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./volumes/sqlite.db"
-    app.run(debug=True, host="0.0.0.0", port="8199")
+    app.run(debug=True, host="0.0.0.0", port="8200")
