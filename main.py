0963b9c0a77817f876735f090ee73c48d74bb058
YLu-1258 <maodou1258@gmail.com>
2023-03-31 11:34:48 -0700

    fixed images py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask_cors import CORS
-from flask import render_template
-from nighthawkguessr_api import app, db
-
+from flask import render_template, url_for
+from nighthawkguessr_api import app, db, project_path
+from pathlib import Path
 from nighthawkguessr_api.api.todo import todo_bp
-
+from flask import send_from_directory
 from nighthawkguessr_api.model.images import initEasyImages
 
 from nighthawkguessr_api.api.leaderboard import leaderboard_bp
@@ -20,6 +20,28 @@ def init_db():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# app.add_url_rule('/photos/<path:filename>', endpoint='photos', view_func=app.send_static_file)
+# @app.route('/')
+# def photo():
+#     image_dir = Path.cwd()/"images/easy"
+#     images_paths = [i.posix() for i in image_dir.iterdir()]
+#     images = [Images("/image/easy/" + image, 250, 250, 1) for image in images_paths]
+#     return render_template('photo1.html')
+
+# @app.route('/')
+# def capture_image(self):
+#     self.cam = cv2.VideoCapture(0)
+#     self.img = self.cam.read()
+#     self.cam.release()
+#     render_template(index.html,ob=self.img)
+
+
+
+@app.route('/static/images/easy/<path:path>')
+def send_report(path):
+    full_filename = project_path.as_posix() + path
+    return render_template("image_template.html", user_image = path) #url_for('static', filename=f'images/easy/{path}')
 
 if __name__ == "__main__":
     cors = CORS(app)
--- nighthawkguessr_api/model/images.py
+++ nighthawkguessr_api/model/images.py
@@ -110,7 +110,7 @@ def initEasyImages():
     db.create_all()
     image_dir = Path.cwd()/"images/easy"
     images_paths = [i.name for i in image_dir.iterdir()]
-    images = [Images("/image/easy/" + image, 250, 250, 1) for image in images_paths]
+    images = [Images("image/easy/" + image, 250, 250, 1) for image in images_paths]
     for image in images:
         try:
             image.create()
