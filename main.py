5961080493ce52cf9f7d1da076ad6a0853ff2ff6
nikhilc3 <nikhilchakravarthula14@gmail.com>
2023-04-17 20:05:50 -0700

    afafaff

--- main.py
+++ main.py
@@ -5,16 +5,22 @@ from pathlib import Path
 from nighthawkguessr_api.api.todo import todo_bp
 from flask import send_from_directory
 from nighthawkguessr_api.model.images import initEasyImages
+from nighthawkguessr_api.api.jwt_routes import jwt_api
+
+app.config['SECRET_KEY'] = 'sdkhjfajkhgfjakhsdflkajndska'
+
+
+
 
 from nighthawkguessr_api.model.leaderboards import init_leaderboards
-
 from nighthawkguessr_api.api.leaderboard import leaderboard_bp
 from nighthawkguessr_api.api.images import images_bp
-
+from nighthawkguessr_api.model.auth import auth
 
 app.register_blueprint(todo_bp)
 app.register_blueprint(leaderboard_bp)
 app.register_blueprint(images_bp)
+app.register_blueprint(jwt_api)
 
 
 @app.before_first_request
@@ -23,7 +29,6 @@ def init_db():
         db.create_all()
         initEasyImages()
         init_leaderboards()
-
 
 @app.route('/')
 def index():
@@ -53,6 +58,6 @@ def send_report(path):
 
 
 if __name__ == "__main__":
-    cors = CORS(app)
+    cors = CORS(app, resources={r"*": {"origins": "*"}})
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./volumes/sqlite.db"
     app.run(debug=True, host="0.0.0.0", port="8200")
