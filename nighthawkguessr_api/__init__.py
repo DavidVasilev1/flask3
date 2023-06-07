89aa91f735ceaedf2f5f0878b5d3f28e97750a69
nikhilc3 <nikhilchakravarthula14@gmail.com>
2023-06-06 19:12:18 -0700

    JWT Fixes

--- nighthawkguessr_api/__init__.py
+++ nighthawkguessr_api/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from pathlib import Path
 import os
--- nighthawkguessr_api/model/user.py
+++ nighthawkguessr_api/model/user.py
@@ -1,10 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 
 
 from nighthawkguessr_api import db
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
